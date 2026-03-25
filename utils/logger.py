37476815259f0dacd91bf0c88006faf256ac2dad
Liruirui <145514308+liruirui321@users.noreply.github.com>
2026-03-25 15:25:10 +0800

    Add utils/logger.py

---
+++ utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+from pathlib import Path
+
+def setup_logger(log_file):
+    logger = logging.getLogger('genome_assembly')
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
